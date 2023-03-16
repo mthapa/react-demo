d2765bd1eece46fe8f0eb3d4c139ac91497c3a50
Madan Thapa <thapamadan@gmail.com>
2023-03-16 09:11:28 +0545

    set selected index

--- src/components/ListGroup.js
+++ src/components/ListGroup.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Des Moines", "Idaho"];
   //items = [];
-  const handleClick = (event) => console.log(event);
+
+  //hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -9,7 +13,15 @@ function ListGroup() {
       {items.length === 0 && <p> No Item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li key={item} className="list-group-item" onClick={handleClick}>
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
